4d78a15e667eb2c3dfd8cbea797cbee0b38dccab
Bravin157 <gb02032018gu@gmail.com>
2024-11-14 23:03:19 -0300

    Positivos e Média

---
+++ C#/Positivos e Média.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+/*
+    Leia 6 valores. Em seguida, mostre quantos destes valores digitados foram positivos. 
+    Na próxima linha, deve-se mostrar a média de todos os valores positivos digitados, com um 
+    dígito após o ponto decimal.
+ */
+
+namespace ExerciciosBeecrowd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, media = 0.0;
+            int contador = 0; ;
+
+            for (int i = 0; i < 6; i++)
+            {
+                n = double.Parse(Console.ReadLine());
+
+                if (n > 0)
+                {
+                    contador += 1;
+                    media += n;
+                }
+            }
+
+            Console.WriteLine(contador + " valores positivos");
+            Console.WriteLine((media / contador).ToString("f1", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
